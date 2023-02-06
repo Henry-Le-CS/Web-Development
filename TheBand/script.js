a8d4edd4c9505f0c709e4541ea65033db4c6cea6
Henry-Le-CS <lehieu2077@gmail.com>
2023-02-06 10:00:39 +0700

    Reupload website The Band

---
+++ TheBand/script.js
@@ -0,0 +1,25 @@
+const buyBtns = document.querySelectorAll('.js-buy-ticket')
+const modal = document.querySelector('.js-modal')
+const modalClose = document.querySelector('.js-close-buy-ticket')
+const modalContainer = document.querySelector('.js-modal-container')
+
+function showBuyTicket(){
+    modal.classList.add('open')
+}
+
+function hideBuyTicket(){
+    modal.classList.remove('open')
+}
+
+for(const buyBtn of buyBtns){
+    buyBtn.addEventListener('click',showBuyTicket)
+}
+
+modalClose.addEventListener('click',hideBuyTicket)
+
+modal.addEventListener('click',hideBuyTicket)
+
+modalContainer.addEventListener('click',function(event){
+    event.stopPropagation()
+})
+
